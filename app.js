7ce59958e4931f30d85b46fca636c43d0d0d6ee7
Danli Denny Deng <danlideng@yahoo.com>
2022-01-17 11:51:05 -0800

    static files got served

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan')
 
 // express app
 const app = express();
@@ -9,6 +10,9 @@ app.listen(3000);
 // register view engine
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
+
+app.use(express.static('public'))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   const blogs = [
